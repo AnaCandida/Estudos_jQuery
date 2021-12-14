8654d3bea0392dbd9ad6712ee3d8f0790f8763d8
Ana Quadros <ana.inovatech@gmail.com>
2021-12-14 14:10:02 -0300

    refatoraçao em functions

--- alura-typer-inicial/alura-typer/public/js/main.js
+++ alura-typer-inicial/alura-typer/public/js/main.js
@@ -1,30 +1,48 @@
-let textForCopy = $(".textForCopy").text();
-let wordNumber= textForCopy.split(/\S+/).length -1;
-let hinnerWordNumber = $("#tamanho_frase");
-
-hinnerWordNumber.text(wordNumber);
-
-var  campo = $(".textInput");
-campo.on("input", function(){
-   var conteudo  = campo.val();
-   var qtdPalavras = conteudo.split(/\S+/).length -1 ;
-   var qtdCaracteres = conteudo.length;
-   var contadorPalavras = $("#wordCounter").text(qtdPalavras)
-    var contadorCaracteres =$("#caracterCounter").text(qtdCaracteres)
+var inicialTime =  $('#tempo_digitacao').text()
+var typeField = $(".textInput");
 
 
+$(document).ready(function(){
+    initTextForCopySize();
+    initCounters();
+    initTimer();
+    $('#restart').click(restart);    
 })
+
+
+function initTextForCopySize(){
+    let textForCopy = $(".textForCopy").text();
+    let wordNumber= textForCopy.split(/\S+/).length -1;
+    let textForCopySize = $("#tamanho_frase");
+    textForCopySize.text(wordNumber);
+}
+
+
+function initCounters(){
+    typeField.on("input", function(){
+       var conteudo  = typeField.val();
+       var qtdPalavras = conteudo.split(/\S+/).length -1 ;
+       var qtdCaracteres = conteudo.length;
+       var contadorPalavras = $("#wordCounter").text(qtdPalavras)
+        var contadorCaracteres =$("#caracterCounter").text(qtdCaracteres)
+    
+    
+    })
+    
+}
+
+function initTimer(){
 
 var tempoRestante =  $('#tempo_digitacao').text()
 
-campo.one("focus", function(){
+typeField.one("focus", function(){
     
   var cronometroID =  setInterval(function(){
         tempoRestante--;
         $('#tempo_digitacao').text(tempoRestante)
        
        if (tempoRestante <1){
-            campo.attr("disabled",true);
+            typeField.attr("disabled",true);
             clearInterval(cronometroID);
         }
 
@@ -33,14 +51,19 @@ campo.one("focus", function(){
     }, 1000);
 })
 
+}
 
-var tempoInicial =  $('#tempo_digitacao').text()
-$('#restart').click(function(){
-    campo.attr("disabled", false);
-    campo.val("");
-    $("#wordCounter").text("0");
-    $("#caracterCounter").text("0");
-    $('#tempo_digitacao').text()
-    $('#tempo_digitacao').text(tempoInicial)
+function restart(){
+    
+        typeField.attr("disabled", false);
+        typeField.val("");
+        $("#wordCounter").text("0");
+        $("#caracterCounter").text("0");
+        $('#tempo_digitacao').text(inicialTime);
+        initTimer();
+        initCounters();
+    
+    
+}
 
-})
+
